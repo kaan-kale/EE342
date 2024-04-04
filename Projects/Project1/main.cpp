3c8e7ef320cf4a5631ac332b93ac718a2011d2a4
kaan-kale <hkaankale1@gmail.com>
2024-04-04 22:01:18 +0300

    Created Matrix struct

--- Projects/Project1/main.cpp
+++ Projects/Project1/main.cpp
@@ -5,6 +5,137 @@
 
 using namespace std;
 
+// Matrix Struct
+struct Matrix {
+    double** data;
+    int rows;
+    int cols;
+};
+
+void readVectorFromFile(const std::string& filename, double*& vector, int& size);
+
+void displayMatrix(Matrix matrix);
+
+void displayVector(double* vector, int length);
+
+void subtractVectors(double* a, double* b, double* result, int length);
+
+void subtractVector(double* x, double* y, int length);
+
+void elementwiseMultiplyVector(double* vector, double scalar, int length);
+
+void copyVector(double* source, double* destination, int length);
+
+void swapVectors(double* a, double* b, int length);
+
+void copyMatrixRow(Matrix matrix, double* destination, int rowIdx);
+
+void copyMatrix(Matrix source, Matrix destination, int numRows, int numCols);
+
+void backSubstitute(Matrix matrix, double* vector, double* result);
+
+Matrix readMatrixFromFile(const std::string& matrixFileName);
+
+int main() {
+    string matrixFileName = "A.txt";
+    string vectorFileName = "b.txt";
+
+    Matrix matrixA;
+    matrixA = readMatrixFromFile(matrixFileName);
+
+    displayMatrix(matrixA);
+
+    double* vectorB = nullptr;
+    int vectorBSize;
+
+    readVectorFromFile(vectorFileName, vectorB, vectorBSize);
+
+    displayVector(vectorB, vectorBSize);
+
+    // Add vector B as a column to the matrix
+    for (int i = 0; i < matrixA.rows; ++i)
+    {
+        matrixA.data[i] = (double*)realloc(matrixA.data[i], (matrixA.cols + 1) * sizeof(double));
+        matrixA.data[i][matrixA.cols] = vectorB[i];
+    }
+
+    displayMatrix(matrixA);
+
+    // Gaussian elimination
+    for (int columnIdx = 0; columnIdx < matrixA.cols; ++columnIdx)
+    {       
+        int maxIdx = 0; 
+        double max = 0, value = 0;
+
+        // Find the max
+        for (int i = columnIdx; i < matrixA.rows; ++i)
+        {   
+            // Check the value
+            value = matrixA.data[i][columnIdx];
+            if (value > max) 
+            { 
+                max = value;
+                maxIdx = i;
+            }
+        }
+
+        double* temp = matrixA.data[columnIdx];
+
+        matrixA.data[columnIdx] = matrixA.data[maxIdx];
+        matrixA.data[maxIdx] = temp;
+
+        std::cout << "Displaying Matrix A:" << endl;
+        displayMatrix(matrixA);
+
+        double cellRatio;
+        for (int i = columnIdx + 1; i < matrixA.rows; ++i)
+        {   
+            // Check the value
+            value = matrixA.data[i][columnIdx];
+            cellRatio = value / max;
+
+            // Multiply the vector by a scalar
+            double vectorToSubtract[matrixA.cols + 1];
+            copyMatrixRow(matrixA, vectorToSubtract, columnIdx);
+            elementwiseMultiplyVector(vectorToSubtract, cellRatio, matrixA.cols + 1);
+
+            // Subtract vectors
+            subtractVector(matrixA.data[i], vectorToSubtract, matrixA.cols + 1);
+        }  
+
+        std::cout << "Displaying Matrix A:" << endl;
+        displayMatrix(matrixA);
+    }
+
+
+    // Back substitution
+    double* result = new double[matrixA.rows];
+
+    // Get vector b from the last column of the matrix
+    for (int i = 0; i < matrixA.rows; ++i)
+    {
+        vectorB[i] = matrixA.data[i][matrixA.cols];
+    }
+
+    std::cout << "Displaying Vector B:" << endl;
+    displayVector(vectorB, matrixA.rows);
+
+    backSubstitute(matrixA, vectorB, result);
+    
+    std::cout << "Displaying Result:" << endl;
+    displayVector(result, matrixA.rows);
+
+    // Deallocate memory which is important for dynamic allocation
+    for (int i = 0; i < matrixA.rows; ++i) {
+        delete[] matrixA.data[i];
+    }
+
+    delete[] matrixA.data;
+    delete [] vectorB;
+    delete [] result;
+    return 0;
+}
+
 // Function to read a vector of doubles from a text file
 void readVectorFromFile(const std::string& filename, double*& vector, int& size) {
     std::ifstream infile(filename);
@@ -42,16 +173,18 @@ void readVectorFromFile(const std::string& filename, double*& vector, int& size)
     size = vectorSize;
 }
 
-void displayMatrix(double** matrix, int rows, int cols)
-{    
+void displayMatrix(Matrix matrix)
+{   
+    int numRows = matrix.rows;
+    int numCols = matrix.cols;
+    double** matrixData = matrix.data;
     // Display the dynamically allocated matrix
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            std::cout << matrix[i][j] << " ";
+    for (int i = 0; i < numRows; ++i) {
+        for (int j = 0; j < numCols; ++j) {
+            std::cout << matrixData[i][j] << " ";
         }
         std::cout << std::endl;
-    }
-}
+    }}
 
 void displayVector(double* vector, int length)
 {
@@ -76,7 +209,7 @@ void subtractVector(double* x, double* y, int length)
     }
 }
 
-void multiplyVector(double* vector, double scalar, int length)
+void elementwiseMultiplyVector(double* vector, double scalar, int length)
 {
     // Multiply the vector by a scalar
     for (int i = 0; i < length; ++i) {
@@ -100,51 +233,44 @@ void swapVectors(double* a, double* b, int length)
     delete[] temp;
 }
 
-void copyMatrixRow(double** source, double* destination, int row, int length)
-{
-    for (int i = 0; i < length; ++i) {
-        destination[i] = source[row][i];
+void copyMatrixRow(Matrix matrix, double* destination, int rowIdx)
+{   
+    for (int i = 0; i < matrix.cols; ++i) {
+        destination[i] = matrix.data[rowIdx][i];
     }
 }
 
-void copyMatrix(double** source, double** destination, int rows, int cols)
-{
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            destination[i][j] = source[i][j];
-        }
+void copyMatrix(Matrix source, Matrix destination, int numRows, int numCols)
+{   
+    // Use copyMatrixRow to copy all rows
+    for (int i = 0; i < numRows; ++i) {
+        copyMatrixRow(source, destination.data[i], i);
     }
 }
 
-void backSubstitute(double** matrix, double* vector, double* result, int rows, int cols)
+void backSubstitute(Matrix matrix, double* vector, double* result)
 {   
     // Understand and implement the back substitution algorithm
-    for (int i = rows - 1; i >= 0; --i)
+    for (int i = matrix.rows - 1; i >= 0; --i)
     {
         double sum = 0;
-        for (int j = i + 1; j < cols; ++j)
+        for (int j = i + 1; j < matrix.cols; ++j)
         {
-            sum += matrix[i][j] * result[j];
+            sum += matrix.data[i][j] * result[j];
         }
-        result[i] = (vector[i] - sum) / matrix[i][i];
+        result[i] = (vector[i] - sum) / matrix.data[i][i];
     }
 }
 
-int main() {
-    string matrixFileName = "A.txt";
-    string vectorFileName = "b.txt";
-    
-    // Make the above part a function
-    // TODO: Read the vector from a file
-
-    // Open the text file
+Matrix readMatrixFromFile(const std::string& matrixFileName) {
     std::ifstream infile(matrixFileName);
     if (!infile) {
         std::cerr << "Failed to open the file." << std::endl;
-        return 1;
+        Matrix errorMatrix = {nullptr, 0, 0};
+        return errorMatrix;
     }
 
-    // Read the matrix dimensions
+    // Determine the dimensions of the matrix
     int rows = 0, cols = 0;
     std::string line;
     while (std::getline(infile, line)) {
@@ -158,9 +284,9 @@ int main() {
     cols /= rows;
 
     // Allocate memory for the matrix
-    double** matrixA = new double*[rows];
+    double** matrixData = new double*[rows];
     for (int i = 0; i < rows; ++i) {
-        matrixA[i] = new double[cols];
+        matrixData[i] = new double[cols];
     }
 
     // Reset file pointer to beginning of the file
@@ -172,100 +298,11 @@ int main() {
         std::getline(infile, line);
         std::istringstream iss(line);
         for (int j = 0; j < cols; ++j) {
-            iss >> matrixA[i][j];
+            iss >> matrixData[i][j];
         }
     }
 
-    displayMatrix(matrixA, rows, cols);
-
-
-    double* vectorB = nullptr;
-    int vectorBSize;
-
-    readVectorFromFile(vectorFileName, vectorB, vectorBSize);
-
-    displayVector(vectorB, vectorBSize);
-
-    // Add vector B as a column to the matrix
-    for (int i = 0; i < rows; ++i)
-    {
-        matrixA[i] = (double*)realloc(matrixA[i], (cols + 1) * sizeof(double));
-        matrixA[i][cols] = vectorB[i];
-    }
-
-    displayMatrix(matrixA, rows, cols + 1);
-
-    // Gaussian elimination
-    for (int columnIdx = 0; columnIdx < cols; ++columnIdx)
-    {       
-        int maxIdx = 0; 
-        double max = 0, value = 0;
-
-        // Find the max
-        for (int i = columnIdx; i < rows; ++i)
-        {   
-            // Check the value
-            value = matrixA[i][columnIdx];
-            if (value > max) 
-            { 
-                max = value;
-                maxIdx = i;
-            }
-        }
-
-        double* temp = matrixA[columnIdx];
-
-        matrixA[columnIdx] = matrixA[maxIdx];
-        matrixA[maxIdx] = temp;
-
-        std::cout << "Displaying Matrix A:" << endl;
-        displayMatrix(matrixA, rows, cols + 1);
-
-        double cellRatio;
-        for (int i = columnIdx + 1; i < rows; ++i)
-        {   
-            // Check the value
-            value = matrixA[i][columnIdx];
-            cellRatio = value / max;
-
-            // Multiply the vector by a scalar
-            double vectorToSubtract[cols + 1];
-            copyMatrixRow(matrixA, vectorToSubtract, columnIdx, cols + 1);
-            multiplyVector(vectorToSubtract, cellRatio, cols + 1);
-
-            // Subtract vectors
-            subtractVector(matrixA[i], vectorToSubtract, cols + 1);
-        }  
-
-        std::cout << "Displaying Matrix A:" << endl;
-        displayMatrix(matrixA, rows, cols + 1);
-    }
-
-
-    // Back substitution
-    double* result = new double[rows];
-
-    // Get vector b from the last column of the matrix
-    for (int i = 0; i < rows; ++i)
-    {
-        vectorB[i] = matrixA[i][cols];
-    }
-
-    std::cout << "Displaying Vector B:" << endl;
-    displayVector(vectorB, rows);
-
-    backSubstitute(matrixA, vectorB, result, rows, cols);
-    
-    std::cout << "Displaying Result:" << endl;
-    displayVector(result, rows);
-
-    // Deallocate memory which is important for dynamic allocation
-    for (int i = 0; i < rows; ++i) {
-        delete[] matrixA[i];
-    }
-
-    delete[] matrixA;
-    delete [] vectorB;
-    delete [] result;
-    return 0;
+    // Create a Matrix struct and return it
+    Matrix matrix = {matrixData, rows, cols};
+    return matrix;
 }
